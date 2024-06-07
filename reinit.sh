b68263f33f2bd9c4912b06d9100cbaf39af8004d
Kyle Booten <kbooten>
2024-06-07 08:33:26 -0400

    manually fixing my overwrite"

--- reinit.sh
+++ reinit.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Copy and overwrite the files
-cp _field_prose_DEFAULT.txt _field_prose_.txt
-cp _field_yaml_DEFAULT.txt _field_yaml_.txt
+# # Copy and overwrite the files
+# cp _field_prose_DEFAULT.txt _field_prose_.txt
+# cp _field_yaml_DEFAULT.txt _field_yaml_.txt
 
 
-# Print a message indicating the files have been overwritten
-echo "Files have been successfully overwritten with the default files."
+# # Print a message indicating the files have been overwritten
+# echo "Files have been successfully overwritten with the default files."
